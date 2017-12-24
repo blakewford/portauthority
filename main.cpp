a8e6bf1d65b4c59c4548698405671559d76111af
Blake W. Ford <blakewford@gmail.com>
2017-12-24 13:28:09 -0600

    Start transition from outline to real application.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <libelf.h>
 
 int32_t cachedArgc = 0;
@@ -22,32 +23,114 @@ int main(int argc, char** argv)
         storagePointer+=(length+1);
     }
 
-    unsigned int version = elf_version(EV_CURRENT);
-    assert(version != EV_NONE);
-
-    Elf* elf = NULL;
-    int executable = 0;
-    if(cachedArgc > 1) executable = open(cachedArgv[1], O_RDONLY, 0);
+    FILE* executable = 0;
+    if(cachedArgc > 1) executable = fopen(cachedArgv[1], "r");
     if(executable)
     {
-        elf = elf_begin(executable, ELF_C_READ, NULL);
-        Elf_Kind ek = elf_kind(elf);
-        switch(ek)
+        fseek(executable, 0, SEEK_END);
+        int32_t size = ftell(executable);
+        rewind(executable);
+        uint8_t* binary = (uint8_t*)malloc(size);
+        size_t read = fread(binary, 1, size, executable);
+        if(read != size) return -1;
+
+        uint64_t offset = 0;
+        uint16_t headerSize = 0;
+        uint16_t numHeaders = 0;
+        uint16_t stringsIndex = 0;
+        if(binary[4] == 0x2)
         {
-            case ELF_K_ELF:
-                break;
-            case ELF_K_NONE:
-                printf("%s is not an elf file.\n", cachedArgv[1]);
-                return -1;
-            case ELF_K_AR:
-            default:
-                assert(0);
+            Elf64_Ehdr* header = (Elf64_Ehdr*)binary;
+            headerSize = header->e_shentsize;
+            numHeaders = header->e_shnum;
+            offset = header->e_shoff;
+            stringsIndex = header->e_shstrndx;
         }
+        else
+        {
+            Elf32_Ehdr* header = (Elf32_Ehdr*)binary;
+            headerSize = header->e_shentsize;
+            numHeaders = header->e_shnum;
+            offset = header->e_shoff;
+            stringsIndex = header->e_shstrndx;
+        }
+
+        struct sectionInfo
+        {
+            uint32_t type;
+            uint32_t index;
+            uint64_t address;
+            uint64_t offset;
+            uint64_t size;
+        };
+
+        struct sections
+        {
+            uint32_t numSections;
+            sectionInfo* si;
+        };
+
+        sections sect;
+        int32_t ndx = 0;
+        sect.si = (sectionInfo*)malloc(sizeof(sectionInfo)*numHeaders);
+        while(numHeaders--)
+        {
+            if(headerSize == sizeof(Elf64_Shdr))
+            {
+                Elf64_Shdr* section = (Elf64_Shdr*)(binary + offset);
+                sect.si[ndx].index = section->sh_name;
+                sect.si[ndx].address = section->sh_addr;
+                sect.si[ndx].type = section->sh_type;
+                sect.si[ndx].offset = section->sh_offset;
+                sect.si[ndx].size = section->sh_size;
+            }
+            else
+            {
+                Elf32_Shdr* section = (Elf32_Shdr*)(binary + offset);
+                sect.si[ndx].index = section->sh_name;
+                sect.si[ndx].address = section->sh_addr;
+                sect.si[ndx].type = section->sh_type;
+                sect.si[ndx].offset = section->sh_offset;
+                sect.si[ndx].size = section->sh_size;
+            }
+
+            offset += headerSize;
+            ndx++;
+        }
+
+        char stringBuffer[sect.si[stringsIndex].size];
+        memcpy(stringBuffer, &binary[sect.si[stringsIndex].offset], sect.si[stringsIndex].size);
+
+        char nameBuffer[64];
+        memset(nameBuffer, '\0', 64);
+
+        ndx = 0;
+        int32_t cursor = 0;
+        uint32_t length = sect.si[stringsIndex].size;
+        printf("0 ");
+        while(length--)
+        {
+            nameBuffer[cursor] = stringBuffer[ndx++];
+            if(nameBuffer[cursor] == '\0')
+            {
+                printf("%s\n", nameBuffer);
+                memset(nameBuffer, '\0', 64);
+                cursor = 0;
+                if(ndx != sect.si[stringsIndex].size)
+                    printf("%x ", ndx);
+            }
+            else
+            {
+                cursor++;
+            }
+        }
+
+
+        free(sect.si);
+        free(binary);
     }
 
     printf("Clustering.\n");
-
-    elf_end(elf);
-    close(executable);
+    fclose(executable);
 
 }
