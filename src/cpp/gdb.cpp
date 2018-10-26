9b81c3997481766a1b92bfec531786d2e3c314e4
Blake W. Ford <blakewford@gmail.com>
2018-10-26 17:12:52 -0500

    Switch over to custom gdb client.

--- src/cpp/gdb.cpp
+++ src/cpp/gdb.cpp
@@ -5,134 +5,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-bool gTimeout = false;
 #define GDB_PORT 1234
 
-void* startTimer(void* arg)
-{
-    int32_t count = *(int32_t*)arg;
-    while(count--)
-    {
-        usleep(1000*1000);
-    }
-
-    gTimeout = true;
-}
-
-void* runProgram(void* argument)
-{
-    char buffer[256];
-    memset(buffer, '\0', 256);
-    sprintf(buffer, "( cat ) | avr-gdb %s -ex \"target remote :1234\"", (const char*)argument);
-    int error = system(buffer);
-}
-
-void profileGdb(const char* executable, uint64_t profilerAddress, uint64_t moduleBound, isa* arch, analyzer** analyzers, int32_t timeout)
-{
-    avr_isa& instructionSet = (avr_isa&)(*arch);
-
-    pthread_t programThread;
-    pthread_create(&programThread, NULL, runProgram, (char*)executable);
-
-    char buffer[1024];
-    char buffer2[1024];
-    int32_t error = 0;
-    int32_t rounds = 0;
-
-    pid_t pid = 0;
-    while(pid == 0)
-    {
-        FILE *cmd;
-        char pidString[6];
-        memset(pidString, '\0', 6);
-        if(rounds % 2 == 0)
-        {
-            cmd = popen("pidof gdb", "r");
-        }
-        else
-        {
-            cmd = popen("pidof avr-gdb", "r");
-        }
-        rounds++;
-        char* value = fgets(pidString, 6, cmd);
-        pid = strtoul(pidString, NULL, 10);
-        pclose(cmd);
-        usleep(0);
-    }
-    memset(buffer, '\0', 1024);
-    sprintf(buffer, "echo set confirm off > /proc/%d/fd/0", pid);
-    error = system(buffer);
-    memset(buffer, '\0', 1024);
-    sprintf(buffer, "echo set logging on > /proc/%d/fd/0", pid);
-    error = system(buffer);
-    memset(buffer, '\0', 1024);
-    sprintf(buffer, "echo set logging redirect on > /proc/%d/fd/0", pid);
-    error = system(buffer);
-    sprintf(buffer, "echo si > /proc/%d/fd/0", pid);
-    sprintf(buffer2, "echo 'x $pc' > /proc/%d/fd/0", pid);
-    pthread_t timerThread;
-    pthread_create(&timerThread, NULL, startTimer, &timeout);
-    while(!gTimeout)
-    {
-        error = system(buffer);
-        error = system(buffer2);
-    }
-    usleep(1*1000*1000);
-    memset(buffer, '\0', 1024);
-    sprintf(buffer, "echo quit > /proc/%d/fd/0", pid);
-    error = system(buffer);
-
-    char line[256];
-    uint32_t opcode = 0x0000; //NOP
-    uint32_t instructionCount = 0;
-    FILE* log = fopen("gdb.txt", "r");
-    while(fgets(line, sizeof(line), log))
-    {
-        if(strstr(line, "<") != NULL && strstr(line, ":") != NULL)
-        {
-            opcode = (uint32_t)strtol(strstr(line, ":")+1, NULL, 16);
-        }
-        else if(strstr(line, "(gdb) 0x") != NULL)
-        {
-            long address = strtol(strstr(line, "0x"), NULL, 16);
-            if(address < moduleBound)
-            {
-                long ndx = instructionSet.find(decode(opcode));
-                if(ndx != -1)
-                {
-                    uint8_t count = NUM_ANALYZERS;
-                    const isa_instr* instruction = instructionSet.get_instr(ndx);
-                    if(!strcmp(instruction->m_mnem, "break"))
-                    {
-                        printf("BREAK\n");
-                        break;
-                    }
-                    while(count--)
-                    {
-                        analyzers[count]->analyze(address, instruction);
-                    }
-                }
-                else
-                {
-                    printf("Not found %s 0x%x\n", decode(opcode), opcode);
-                }
-                opcode = 0x000; //NOP
-                instructionCount++;
-            }
-        }
-        if(strstr(line, "__stop_program") != NULL)
-        {
-            break;
-        }
-    }
-    fclose(log);
-    remove("gdb.txt");
-    printf("%u\n", instructionCount);
-}
-
 char gLastPacket[64];
-bool packetRead(int fd)
+bool packetRead(int fd, uint32_t& address)
 {
+    address = 0;
+
     char byte;
     uint8_t ndx = 0;
     char buffer[64];
@@ -151,6 +30,12 @@ bool packetRead(int fd)
         return false;
     }
 
+    if(strstr(buffer, "T05") != nullptr)
+    {
+        std::string message(buffer);
+        address = bswap_32(strtol(message.substr(message.find_last_of(":")+1).c_str(), nullptr, 16));
+    }
+
     strcpy(gLastPacket, buffer);
 
     char checksum[2];
@@ -159,47 +44,76 @@ bool packetRead(int fd)
     return true;
 }
 
-bool packetWrite(int fd, const char* replay)
+bool packetWrite(int fd, uint32_t& address, const char* replay)
 {
     ssize_t err = 0;
     err = write(fd, replay, strlen(replay));
-    return packetRead(fd);
+    return packetRead(fd, address);
 }
 
-void singleStep()
+void profileGdb(const char* executable, uint64_t profilerAddress, uint64_t moduleBound, isa* arch, analyzer** analyzers, int32_t timeout)
 {
+    avr_isa& instructionSet = (avr_isa&)(*arch);
+
     int32_t fd;
-    struct sockaddr_in address;
+    struct sockaddr_in socketAddress;
 
     fd = socket(AF_INET, SOCK_STREAM, 0);
     if(fd == 0)
         return;
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(GDB_PORT);
+    socketAddress.sin_family = AF_INET;
+    socketAddress.sin_addr.s_addr = INADDR_ANY;
+    socketAddress.sin_port = htons(GDB_PORT);
 
-    if(connect(fd, (struct sockaddr*)&address, sizeof(address)) == -1)
+    if(connect(fd, (struct sockaddr*)&socketAddress, sizeof(socketAddress)) == -1)
         return;
 
-    const char* replay = "";
-
+    uint32_t address = 0;
     //replay sniffed initialization packets
-    packetWrite(fd, "+$qSupported:multiprocess+;swbreak+;hwbreak+;qRelocInsn+#c9");
-    packetWrite(fd, "+$Hg0#df");
-    packetWrite(fd, "+$qTStatus#49");
-    packetWrite(fd, "+$?#3f");
-    packetWrite(fd, "+$qfThreadInfo#bb");
-    packetWrite(fd, "+$qL1160000000000000000#55");
-    packetWrite(fd, "+$Hc-1#09");
-    packetWrite(fd, "+$qC#b4");
-    packetWrite(fd, "+$qAttached#8f");
-    packetWrite(fd, "+$qOffsets#4b");
-    packetWrite(fd, "+$qL1160000000000000000#55");
-    packetWrite(fd, "+$qSymbol::#5b");
+    packetWrite(fd, address, "+$qSupported:multiprocess+;swbreak+;hwbreak+;qRelocInsn+#c9");
+    packetWrite(fd, address, "+$Hg0#df");
+    packetWrite(fd, address, "+$qTStatus#49");
+    packetWrite(fd, address, "+$?#3f");
+    packetWrite(fd, address, "+$qfThreadInfo#bb");
+    packetWrite(fd, address, "+$qL1160000000000000000#55");
+    packetWrite(fd, address, "+$Hc-1#09");
+    packetWrite(fd, address, "+$qC#b4");
+    packetWrite(fd, address, "+$qAttached#8f");
+    packetWrite(fd, address, "+$qOffsets#4b");
+    packetWrite(fd, address, "+$qL1160000000000000000#55");
+    packetWrite(fd, address, "+$qSymbol::#5b");
 
-    while(packetWrite(fd, "+$s#73"))
-        ;
+    uint32_t opcode = 0x0000; //NOP
+    uint32_t instructionCount = 0;
+    while(packetWrite(fd, address, "+$s#73"))
+    {
+        if(address < moduleBound)
+        {
+            long ndx = instructionSet.find(decode(opcode));
+            if(ndx != -1)
+            {
+                uint8_t count = NUM_ANALYZERS;
+                const isa_instr* instruction = instructionSet.get_instr(ndx);
+                if(!strcmp(instruction->m_mnem, "break"))
+                {
+                    printf("BREAK\n");
+                    break;
+                }
+                while(count--)
+                {
+                    analyzers[count]->analyze(address, instruction);
+                }
+            }
+            else
+            {
+                printf("Not found %s 0x%x\n", decode(opcode), opcode);
+            }
+            opcode = 0; //NOP
+            instructionCount++;
+        }
+    }
 
+    printf("%u\n", instructionCount);
     close(fd);
 }
