6f0777adeb3c43870fd25543c836ad0b6a6f1b13
Blake W. Ford <blakewford@gmail.com>
2018-11-03 15:45:20 -0500

    Get code coverage running on the raspberry pi.

--- src/cpp/gdb.cpp
+++ src/cpp/gdb.cpp
@@ -10,15 +10,18 @@
 
 #define GDB_PORT 1234
 
-char gLastPacket[256];
+#define MAX_PACKET_SIZE 0x4000
+
+uint8_t gMachine = 0;
+char gLastPacket[MAX_PACKET_SIZE];
 bool packetRead(int fd, uint32_t& value)
 {
     value = 0;
 
     char byte;
     uint8_t ndx = 0;
-    char buffer[256];
-    memset(buffer, '\0', 256);
+    char buffer[MAX_PACKET_SIZE];
+    memset(buffer, '\0', MAX_PACKET_SIZE);
 
     ssize_t err = 0;
     err = read(fd, &byte, 1); //ack
@@ -36,7 +39,16 @@ bool packetRead(int fd, uint32_t& value)
     if(strstr(buffer, "T05") != nullptr)
     {
         std::string message(buffer);
-        value = bswap_32(strtol(message.substr(message.find_last_of(":")+1).c_str(), nullptr, 16));
+        if(gMachine == EM_AVR)
+        {
+            value = bswap_32(strtol(message.substr(message.find_last_of(":")+1).c_str(), nullptr, 16));
+        }
+        else
+        {
+            message = message.substr(message.find(";20:")+4);
+            value = bswap_32(strtol(message.substr(0, message.find_first_of("*")).c_str(), nullptr, 16));
+            value = value >> 8;
+        }
     }
     else if(strstr(buffer, "$") != nullptr && strstr(buffer, "#"))
     {
@@ -70,6 +82,7 @@ uint32_t profileGdb(const char* executable, uint8_t machine, uint64_t profilerAd
     if(fd == 0)
         return 0;
 
+    gMachine = machine;
     socketAddress.sin_family = AF_INET;
     if(machine == EM_AVR)
     {
@@ -77,7 +90,7 @@ uint32_t profileGdb(const char* executable, uint8_t machine, uint64_t profilerAd
     }
     else
     {
-        strcpy((char*)&socketAddress.sin_addr.s_addr, (char*)gethostbyname("raspberrypi")->h_addr);
+        strncpy((char*)&socketAddress.sin_addr.s_addr, (char*)gethostbyname("raspberrypi")->h_addr, sizeof(in_addr_t));
     }
     socketAddress.sin_port = htons(GDB_PORT);
 
@@ -100,6 +113,29 @@ uint32_t profileGdb(const char* executable, uint8_t machine, uint64_t profilerAd
         packetWrite(fd, address, "+$qOffsets#4b");
         packetWrite(fd, address, "+$qL1160000000000000000#55");
         packetWrite(fd, address, "+$qSymbol::#5b");
+    }
+    else
+    {
+        packetWrite(fd, address, "+$qSupported:multiprocess+;swbreak+;hwbreak+;qRelocInsn+;fork-events+;vfork-events+;exec-events+;vContSupported+;QThreadEvents+;no-resumed+;xmlRegisters=i386#6a");
+        packetWrite(fd, address, "+$QProgramSignals:0;1;3;4;6;7;8;9;a;b;c;d;e;f;10;11;12;13;14;15;16;17;18;19;1a;1b;1c;1d;1e;1f;20;21;22;23;24;25;26;27;28;29;2a;2b;2c;2d;2e;2f;30;31;32;33;34;35;36;37;38;39;3a;3b;3c;3d;3e;3f;40;41;42;43;44;45;46;47;48;49;4a;4b;4c;4d;4e;4f;50;51;52;53;54;55;56;57;58;59;5a;5b;5c;5d;5e;5f;60;61;62;63;64;65;66;67;68;69;6a;6b;6c;6d;6e;6f;70;71;72;73;74;75;76;77;78;79;7a;7b;7c;7d;7e;7f;80;81;82;83;84;85;86;87;88;89;8a;8b;8c;8d;8e;8f;90;91;92;93;94;95;96;97;#75");
+        packetWrite(fd, address, "+$Hgp0.0#ad");
+        packetWrite(fd, address, "+$qXfer:features:read:target.xml:0,fff#7d");
+        packetWrite(fd, address, "+$qXfer:features:read:aarch64-core.xml:0,fff#35");
+        packetWrite(fd, address, "+$qXfer:features:read:aarch64-fpu.xml:0,fff#d7");
+        packetWrite(fd, address, "+$qXfer:auxv:read::0,1000#6b");
+        packetWrite(fd, address, "+$QNonStop:0#8c");
+        packetWrite(fd, address, "+$qTStatus#49");
+        packetWrite(fd, address, "+$qTfV#81");
+        packetWrite(fd, address, "+$qTsV#8e");
+        packetWrite(fd, address, "+$?#3f");
+        packetWrite(fd, address, "+$qXfer:threads:read::0,fff#03");
+        packetWrite(fd, address, "+$qAttached#8f");
+        packetWrite(fd, address, "+$Hc-1#09");
+        packetWrite(fd, address, "+$qOffsets#4b");
+        packetWrite(fd, address, "+$G0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0f6ffff7f000000c02cfdb77f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000#93");
+        packetWrite(fd, address, "+$Z0,402958,4#52");
+        packetWrite(fd, address, "+$vCont;c#a8");
+        packetWrite(fd, address, "+$z0,402958,4#72");
     }
 
     uint32_t opcode = 0x0000; //NOP
@@ -133,9 +169,13 @@ uint32_t profileGdb(const char* executable, uint8_t machine, uint64_t profilerAd
             checksum += '4';
 
             //+$mce,4#95
+            long ndx = -1;
             sprintf(buffer, "+$m%x,4#%x\n", address, checksum%256);
-            packetWrite(fd, opcode, buffer);
-            long ndx = instructionSet.find(decode(opcode));
+            if(machine == EM_AVR)
+            {
+                packetWrite(fd, opcode, buffer);
+                ndx = instructionSet.find(decode(opcode));
+            }
             if(ndx != -1)
             {
                 uint8_t count = NUM_ANALYZERS;
@@ -163,6 +203,7 @@ uint32_t profileGdb(const char* executable, uint8_t machine, uint64_t profilerAd
         }
         else
         {
+            usleep(1000*32);
             keepGoing = packetWrite(fd, address, "+$vCont;s#b8");
         }
     }
