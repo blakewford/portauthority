e50ffd14e8bc8189bfd37f7853f07dba1da957cc
Blake W. Ford <blakewford@gmail.com>
2020-07-26 14:04:29 -0500

    Update python source

---
+++ src/python/step.py
@@ -0,0 +1,18 @@
+import lldb
+import binascii
+
+def steploop(debugger, unused0, unused1, unused2):
+  target = debugger.GetSelectedTarget()
+  process = target.GetProcess()
+  thread = process.GetSelectedThread()
+  frame = thread.GetFrameAtIndex(0)
+
+  value = ""
+  while str(value) != "No value":
+    value = frame.FindRegister("pc")
+    if str(value) != "No value":
+      error = lldb.SBError()
+      bytes = process.ReadMemory(value.GetValueAsUnsigned(), 8, error)
+      thread.StepInstruction( True )
+      print(hex(value.GetValueAsUnsigned())[2:] + ":" + str(binascii.hexlify(bytes))[2:-1])
+    
