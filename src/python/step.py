8064fcc9279fc3d1babd6a4f80371afea40dc90f
Blake W. Ford <blakewford@gmail.com>
2020-07-26 23:00:12 -0500

    Update stepping script

--- src/python/step.py
+++ src/python/step.py
@@ -5,14 +5,17 @@ def steploop(debugger, unused0, unused1, unused2):
   target = debugger.GetSelectedTarget()
   process = target.GetProcess()
   thread = process.GetSelectedThread()
-  frame = thread.GetFrameAtIndex(0)
 
   value = ""
   while str(value) != "No value":
+    frame = thread.GetFrameAtIndex(0)
     value = frame.FindRegister("pc")
     if str(value) != "No value":
       error = lldb.SBError()
-      bytes = process.ReadMemory(value.GetValueAsUnsigned(), 8, error)
-      thread.StepInstruction( True )
-      print(hex(value.GetValueAsUnsigned()) + " : 0x" + str(binascii.hexlify(bytes))[2:-1])
-    
+      address = value.GetValueAsUnsigned()
+      bytes = process.ReadMemory(address, 8, error)
+      print(hex(address) + " : 0x" + str(binascii.hexlify(bytes))[:-1])
+#      if (address >= 0x400520) and (address <= 0x40053c): .plt
+#        thread.StepOut()
+#      else:
+      thread.StepInstruction(False)
