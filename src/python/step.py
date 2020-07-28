502039bcf66954086d2995c761c5e5a41a6d0b08
Blake W. Ford <blakewford@gmail.com>
2020-07-27 22:49:22 -0500

    Automatically pull plt range

--- src/python/step.py
+++ src/python/step.py
@@ -6,6 +6,10 @@ def steploop(debugger, unused0, unused1, unused2):
   target = debugger.GetSelectedTarget()
   process = target.GetProcess()
   thread = process.GetSelectedThread()
+  module = target.GetModuleAtIndex(0)
+
+  linkTable = module.FindSection(".plt")
+  tableAddress = linkTable.GetFileAddress()
 
   value = ""
   replay = ""
@@ -17,10 +21,10 @@ def steploop(debugger, unused0, unused1, unused2):
       address = value.GetValueAsUnsigned()
       bytes = process.ReadMemory(address, 8, error)
       replay += hex(value.GetValueAsUnsigned()) + " : 0x" + str(binascii.hexlify(bytes))[:-1] + "\n"
-#      if (address >= 0x400520) and (address <= 0x40053c):
-#        thread.StepOut()
-#      else:
-      thread.StepInstruction(False)
+      if (address >= tableAddress) and (address <= tableAddress + linkTable.GetByteSize()):
+        thread.StepOut()
+      else:
+        thread.StepInstruction(False)
 
   f = open(str(datetime.now()).replace(' ',''), "w")
   f.write(replay)
